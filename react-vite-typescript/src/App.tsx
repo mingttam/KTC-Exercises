889a950d1d1ba59f80596f7e231740db5edb2bd9
mingttam <mingtam.713@gmail.com>
2025-07-09 22:04:01 +0700

    Day 8: React Form Hook

--- react-vite-typescript/src/App.tsx
+++ react-vite-typescript/src/App.tsx
@@ -1,7 +1,13 @@
 import Day8Afternoon from "./components/Day8/afternoon";
+import UserRegistrationForm from "./components/Day8/homework/UserRegistrationForm";
 
 function App() {
-  return <Day8Afternoon />;
+  return (
+    <>
+      <Day8Afternoon />
+      <UserRegistrationForm />
+    </>
+  );
 }
 
 export default App;
--- react-vite-typescript/src/components/Day8/afternoon/index.tsx
+++ react-vite-typescript/src/components/Day8/afternoon/index.tsx
@@ -1,10 +1,12 @@
 import SignPage from "./Ex1";
+import RegisterForm from "./Ex2/RegisterForm";
 import LoginForm from "./Ex3/LoginForm";
 
 const Day8Afternoon = () => {
   return (
     <>
       <SignPage />
+      <RegisterForm />
       <LoginForm />
     </>
   );
